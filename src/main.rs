bc0e07494ba91de1f6fa06cbf0348a21589200c8
AlianZip <vovak01.01@gmail.com>
2024-04-02 00:02:56 +0300

    add defoult discord bot and config

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+#![allow(non_snake_case)]
+
+use std::env;
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+use std::fs;
+use serde_json;
+
+#[allow(dead_code)]
+struct Config {
+    TOKEN: String,
+}
+
+#[allow(dead_code)]
+fn config_reader() -> Config {
+    let res = include_str!("config.json");
+    Config{ TOKEN: serde_json::from_str(res).unwrap() }
+}
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {why:?}");
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token  = config_reader().TOKEN;
+    
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client =
+        Client::builder(&token, intents).event_handler(Handler).await.unwrap();
+
+    // Start listening for events by starting a single shard
+    if let Err(_) = client.start().await {
+       todo!();
+    }
+}
+
+
